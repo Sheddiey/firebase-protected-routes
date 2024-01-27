abe0730aa46cdc2ba223d32a2df7768610b3a232
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2024-01-27 14:42:56 +0300

    created 3 different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from "react"
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Signin from "./Components/Signin";
+import Signup from "./Components/Signup";
+import Account from "./Components/Account";
 
-React
+React;
 const App = () => {
   return (
-    <div>
-      
+    <div className="">
+      <h1 className="text-center text-3xl font-bold">
+        Firebase Auth & Context
+      </h1>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
