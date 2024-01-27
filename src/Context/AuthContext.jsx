bfc2dacbe62861a65953d5ad1777717ad0d02260
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2024-01-27 18:04:27 +0300

    sign in user completed

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,17 +1,29 @@
+// eslint-disable-next-line no-unused-vars
 import React, { createContext, useContext, useEffect, useState } from "react";
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { auth } from "../firebase";
 
 const UserContext = createContext();
 
+// eslint-disable-next-line react/prop-types
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
 
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  const logout = () => {
+    return signOut(auth)
   };
 
   useEffect(() => {
@@ -25,7 +37,7 @@ export const AuthContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <UserContext.Provider value={{ createUser, user }}>
+    <UserContext.Provider value={{ createUser, user, logout, signIn }}>
       {children}
     </UserContext.Provider>
   );
